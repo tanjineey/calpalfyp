09b4df1af577848bd9ea9407fbdcfa0a617fc4df
tanjineey <jeraltanjy@gmail.com>
2022-03-23 18:22:07 +0800

    editing req query

--- index.js
+++ index.js
@@ -13,21 +13,24 @@ res.status(200).json({message:'ok'})
 });
 const user = '205550056'; 
 
-async function displaydata(user){
-  var date = Date.now()/1000;
-  var meetingreply = await avatar.getUpcomingMeeting(user,date);
-    var assignreply = await avatar.getUpcomingAssignments(user,date);
-    var examreply = await avatar.getUpcomingExam(user,date);
-    var quizzesreply = await avatar.getUpcomingQuiz(user,date);
-    console.log(meetingreply.toString());
-}
-displaydata(user);
+// async function displaydata(user){
+//   var date = Date.now()/1000;
+//   var meetingreply = await avatar.getUpcomingMeeting(user,date);
+//     var assignreply = await avatar.getUpcomingAssignments(user,date);
+//     var examreply = await avatar.getUpcomingExam(user,date);
+//     var quizzesreply = await avatar.getUpcomingQuiz(user,date);
+//     console.log(meetingreply.toString());
+// }
+// displaydata(user);
 
 app.set('view engine', 'ejs');
 app.get('/',async(req,res)=>{
-  const {id} = req.query.id;
+  const id = req.query.id;
+    var date = Date.now()/1000;
    const meetingreply  = await avatar.getUpcomingMeeting(id,date);
-   res.render('pages/index', { meetingreply })
+   var quizzesreply = await avatar.getUpcomingQuiz(id,date);
+
+   res.render('pages/index', { meetingreply, id, quizzesreply})
   // res.render('pages/index');
 })
 // app.get('/', (req,res)=>{
